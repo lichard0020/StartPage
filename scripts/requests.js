497331e87e6cfd8c1e9686768aeaf7197e568991
lichard0020 <64459335+lichard0020@users.noreply.github.com>
2022-10-06 11:23:59 +0800

    Update requests.js

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,13 +1,12 @@
 var httpRequest = new XMLHttpRequest();
-httpRequest.open("GET", 'https://api.dujin.org/bing/1920.php', true);
+httpRequest.open("GET", 'https://api.no0a.cn/api/bing/day', true);
 httpRequest.send();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readySte == 4 && httpRequest.status == 200) {
             var json = httpRequest.responseText;
             var pic = eval("(" + json + ")");
             console.log(pic);
-            document.body.style.backgroundImage = "url(" + pic.bing.url + ")";
-            docdument.querySelector(".downloadImage").href = pic.bing.url
-            document.querySelector(".downloadImage").innerHTML = pic.bing.copyright;
+            document.body.style.backgroundImage = "url(" + "https://api.mfstudio.cc/bing/" + ")";
+            docdument.querySelector(".downloadImage").href = "https://api.mfstudio.cc/bing/"
         }
     }

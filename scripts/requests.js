1e7618e7d3de9b561121f267685c2ee76fe61952
lichard0020 <64459335+lichard0020@users.noreply.github.com>
2023-08-17 11:49:02 +0800

    Update requests.js

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,12 +1,12 @@
 var httpRequest = new XMLHttpRequest();
-httpRequest.open("GET", 'https://api.no0a.cn/api/bing/day', true);
+httpRequest.open("GET", 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', true);
 httpRequest.send();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readySte == 4 && httpRequest.status == 200) {
             var json = httpRequest.responseText;
             var pic = eval("(" + json + ")");
             console.log(pic);
-            document.body.style.backgroundImage = "url(" + "https://api.mfstudio.cc/bing/" + ")";
-            docdument.querySelector(".downloadImage").href = "https://api.mfstudio.cc/bing/"
+            document.body.style.backgroundImage = "url(" + "https://cn.bing.com" + pic.url + ")";
+            docdument.querySelector(".downloadImage").href = "https://cn.bing.com"+pic.url;
         }
     }

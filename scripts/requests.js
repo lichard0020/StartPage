c380d475d53966f3c28a5fc095bd237a432e9042
lichard0020 <64459335+lichard0020@users.noreply.github.com>
2023-08-17 09:51:20 +0000

    asefasdf

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,12 +1,11 @@
-var httpRequest = new XMLHttpRequest();
-httpRequest.open("GET", 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', true);
-httpRequest.send();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readySte == 4 && httpRequest.status == 200) {
-            var json = httpRequest.responseText;
-            var pic = eval("(" + json + ")");
-            console.log(pic);
-            document.body.style.backgroundImage = "url(https://api.dujin.org/bing/1920.php)";
-            docdument.querySelector(".downloadImage").href = "https://cn.bing.com"+pic.url;
-        }
-    }
+// var httpRequest = new XMLHttpRequest();
+// httpRequest.open("GET", 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', true);
+// httpRequest.send();
+//     httpRequest.onreadystatechange = function () {
+//         if (httpRequest.readySte == 4 && httpRequest.status == 200) {
+//             var json = httpRequest.responseText;
+//             var pic = eval("(" + json + ")");
+//             console.log(pic);
+//             document.body.style.backgroundImage = "url(https://api.dujin.org/bing/1920.php)";
+//         }
+//     }

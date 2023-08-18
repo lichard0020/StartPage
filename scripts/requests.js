85297771cb8477a343eb4ebfe3f5276e4c3acee3
lichard0020 <64459335+lichard0020@users.noreply.github.com>
2023-08-18 01:12:28 +0000

    123

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,11 +1,7 @@
-// var httpRequest = new XMLHttpRequest();
-// httpRequest.open("GET", 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', true);
-// httpRequest.send();
-//     httpRequest.onreadystatechange = function () {
-//         if (httpRequest.readySte == 4 && httpRequest.status == 200) {
-//             var json = httpRequest.responseText;
-//             var pic = eval("(" + json + ")");
-//             console.log(pic);
-//             document.body.style.backgroundImage = "url(https://api.dujin.org/bing/1920.php)";
-//         }
-//     }
+// const Http = new XMLHttpRequest();
+// const url="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN";
+// Http.open("GET",url);
+// Http.send();
+// Http.onreadystatechange=(e)=>{
+//     console.log(Http.responseText);
+// }
